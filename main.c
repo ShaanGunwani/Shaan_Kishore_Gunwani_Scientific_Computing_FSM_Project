e379abdec5d510a05b5e4c8ad2dffc675eb41e05
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2023-01-21 16:31:55 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/* In this code, the main function continuously reads an incoming bit from the user and calls the "monitor_bits" function from the mylib.c file with this incoming bit as an argument.
+Then it calls the "get_err" function from the mylib.c file, and if it returns ERROR, it prints "Error detected!" to the console. */ 
+
+/* The state of the finite state machine is not explicitly represented in the main function, 
+but it is being handled by the monitor_bits function in the mylib.c file. 
+The state of the machine is represented by variables such as Q2_cur, Q1_cur, and Q0_cur, 
+these variables are used to store the current state of the machine and are being updated in the monitor_bits function, 
+based on the incoming bits and the current state of the machine. 
+The monitor_bits function also contains a switch statement that handles the different states of the machine, 
+and updates the state variables based on the input and the current state. */
+
+
+
+
 #include <stdio.h>
 #include "mylib.h"
 
@@ -15,5 +29,3 @@ int main() {
 }
 
 
-/* In this code, the main function continuously reads an incoming bit from the user and calls the "monitor_bits" function from the mylib.c file with this incoming bit as an argument.
-Then it calls the "get_err" function from the mylib.c file, and if it returns ERROR, it prints "Error detected!" to the console. */ 
