7d06c9cd6a9bddf6066e5fc3853c7e17af143a98
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2023-01-21 16:52:46 +0700

    Update mylib.c

--- my.lib/mylib.c
+++ my.lib/mylib.c
@@ -1,3 +1,14 @@
+/* The mylib.c file is a source file that contains the implementation of the functions that are declared in the mylib.h file. 
+These functions are intended to be used in other source files, such as the main function. 
+In this case, the mylib.c file contains the implementation of the two functions "monitor_bits" and "get_err" that are declared in the mylib.h file. 
+The monitor_bits function is responsible for monitoring the incoming bit stream and updating the state of the finite state machine. 
+The get_err function is used to check the current state of the machine and check if an error occurred in the transmitter or not. 
+The monitor_bits function takes an input parameter Din, which is the incoming bit, 
+and it updates the current state variables Q2_cur, Q1_cur, Q0_cur, Q2_nxt, Q1_nxt, Q0_nxt based on the current state of the machine and the incoming bit. 
+It also updates the err variable based on the current state of the machine and the incoming bit.*/
+
+
+
 #include "mylib.h"
 
 int Q2_cur = 0;
