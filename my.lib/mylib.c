af036b1d18a3b7ca184b3c21aba5fa7757b349b2
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2023-01-21 16:22:05 +0700

    Update mylib.c

--- my.lib/mylib.c
+++ my.lib/mylib.c
@@ -1,18 +1,46 @@
 #include "mylib.h"
 
-#define BIT_WIDTH 3 // number of bits per data group
-int shift_register = 0; // storage for incoming bits
-int err = 0; // signal to indicate error
+int Q2_cur = 0;
+int Q1_cur = 0;
+int Q0_cur = 0;
+int Q2_nxt = 0;
+int Q1_nxt = 0;
+int Q0_nxt = 0;
+int err = NO_ERROR;
 
-void monitor_bits(int incoming_bit) {
-    shift_register = (shift_register << 1) | incoming_bit; // shift in new bit
-    if ((shift_register & 7) == 7) { // check for sequence 111
-        err = 1; // assert ERR signal
-    } else {
-        err = 0; // keep ERR signal clear
+void monitor_bits(int Din) {
+    Q2_cur = Q2_nxt;
+    Q1_cur = Q1_nxt;
+    Q0_cur = Q0_nxt;
+    if (Din == 1) {
+        switch (Q2_cur * 4 + Q1_cur * 2 + Q0_cur) {
+            case 0:
+                Q2_nxt = 0;
+                Q1_nxt = 0;
+                Q0_nxt = 1;
+                break;
+            case 2:
+                Q2_nxt = 0;
+                Q1_nxt = 1;
+                Q0_nxt = 0;
+                break;
+            case 3:
+                Q2_nxt = 0;
+                Q1_nxt = 1;
+                Q0_nxt = 1;
+                err = ERROR;
+                break;
+            case 4:
+                Q2_nxt = 1;
+                Q1_nxt = 0;
+                Q0_nxt = 0;
+                break;
+            case 7:
+                Q2_nxt = 0;
+                Q1_nxt = 0;
+                Q0_nxt = 0;
+                break;
+            default:
+                break;
+        }
     }
-}
-
-int get_err(void) {
-    return err;
-}
