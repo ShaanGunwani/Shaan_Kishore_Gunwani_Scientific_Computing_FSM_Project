cd31eabafd72c835d19ea8c71bfb39cac86783ee
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2023-01-25 19:21:01 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,57 @@
-//In this code, the main function continuously reads an incoming bit from the user and calls the "monitor_bits" function from the mylib.c file with this incoming bit as an argument.
-//Then it calls the "get_err" function from the mylib.c file, and if it returns ERROR, it prints "Error detected!" to the console.
-
-//The state of the finite state machine is not explicitly represented in the main function, 
-//but it is being handled by the monitor_bits function in the mylib.c file. 
-//The state of the machine is represented by variables such as Q2_cur, Q1_cur, and Q0_cur, 
-//these variables are used to store the current state of the machine and are being updated in the monitor_bits function, 
-//based on the incoming bits and the current state of the machine. 
-//The monitor_bits function also contains a switch statement that handles the different states of the machine, 
-//and updates the state variables based on the input and the current state. */
-
-
-
-
 #include <stdio.h>
-#include "mylib.h"
+
+#define NO_ERROR 0
+#define ERROR 1
+
+// Declare variables for the current state
+int Q2_cur = 0;
+int Q1_cur = 0;
+int Q0_cur = 0;
+
+// Declare variable for the error signal
+int ERR = 0;
+
+void monitor_bits(int Din) {
+    // Update the next state variables
+    int Q2_nxt = Q2_cur;
+    int Q1_nxt = Q1_cur;
+    int Q0_nxt = Q0_cur;
+    ERR = 0; // initialize ERR to 0
+
+    // Implement state transition logic
+    if (Din == 1) {
+        Q2_nxt = Q1_cur;
+        Q1_nxt = Q0_cur;
+        Q0_nxt = 1;
+        if (Q2_nxt == 1 && Q1_nxt == 1 && Q0_nxt == 1) {
+            ERR = 1;
+        }
+    } else {
+        Q2_nxt = 0;
+        Q1_nxt = 0;
+        Q0_nxt = 0;
+    }
+
+    // Update the current state variables
+    Q2_cur = Q2_nxt;
+    Q1_cur = Q1_nxt;
+    Q0_cur = Q0_nxt;
+}
+
 
 int main() {
     int Din;
-    while (1) { // infinite loop to continuously read incoming bits
-        printf("Enter the incoming bit: ");
+    while (1) {
+        printf("Enter the input value for Sequence Detector: "); 
         scanf("%d", &Din);
         monitor_bits(Din);
-        if (get_err() == ERROR) {
-            printf("Error detected!\n");
+
+        if (ERR == NO_ERROR) {
+            printf("Output = 0 (No Error)\n");
+        } else {
+            printf("Output = ERR (1)\n");
         }
     }
     return 0;
 }
-
 
